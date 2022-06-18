bf7dd67143269d1c7f5eb6f7a61837e5f769e797
AndreyGureev <daxz.abdrfo.ru5@gmail.com>
2022-06-18 18:52:46 +0400

    Push-Sender

---
+++ src/main/kotlin/ru/netology/pusher/main.kt
@@ -0,0 +1,44 @@
+package ru.netology.pusher
+
+import com.google.auth.oauth2.GoogleCredentials
+import com.google.firebase.FirebaseApp
+import com.google.firebase.FirebaseOptions
+import com.google.firebase.messaging.FirebaseMessaging
+import com.google.firebase.messaging.Message
+import java.io.FileInputStream
+
+
+fun main() {
+    val options = FirebaseOptions.builder()
+        .setCredentials(GoogleCredentials.fromStream(FileInputStream("fcm.json")))
+        .build()
+
+    FirebaseApp.initializeApp(options)
+
+    val likeMessage = Message.builder()
+        .putData("action", "LIKE")
+        .putData("content", """{
+          "userId": 1,
+          "userName": "Andrey",
+          "postId": 2,
+          "postAuthor": "Netology"
+        }""".trimIndent())
+        .setToken("clzRv0XWR1iU5Hl399Yk1W:APA91bEND5RFl6dmKC-m3w6pEPNe6FHD2OoE1As4cLi6SXd1Iz_bKug_41J29QKTb4R0SosKikqBQapRvzM74gBjENRccMQY9SaXKcPCIPZFVhDv5Ggu_JYdk0mWJdimNAn9Bq2Sy3oZ")
+        .build()
+
+    val newPostMessage = Message.builder()
+        .putData("action", "NEW_POST")
+        .putData("content", """{
+          "postId": 1,
+          "postAuthorId": "1",
+          "postAuthor": "Netology",
+          "content": "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов.Но самое важное остаётся с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия — помочь встать на путь роста и начать цепочку перемен - http://netolo.gy/fyb"
+        }""".trimIndent())
+        .setToken("clzRv0XWR1iU5Hl399Yk1W:APA91bEND5RFl6dmKC-m3w6pEPNe6FHD2OoE1As4cLi6SXd1Iz_bKug_41J29QKTb4R0SosKikqBQapRvzM74gBjENRccMQY9SaXKcPCIPZFVhDv5Ggu_JYdk0mWJdimNAn9Bq2Sy3oZ")
+        .build()
+
+
+
+    FirebaseMessaging.getInstance().send(newPostMessage)
+    FirebaseMessaging.getInstance().send(likeMessage)
+}
